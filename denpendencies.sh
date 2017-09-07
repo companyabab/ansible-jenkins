c5be8da4b937cb5c42879467a3e842164184e1a1
root <root@ansible.c.compact-factor-178122.internal>
2017-09-07 18:59:24 +0000

    added connect

---
+++ denpendencies.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+sudo yum install wget 
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins
+sudo service jenkins status
+sudo service jenkins start
+sudo service jenkins status
